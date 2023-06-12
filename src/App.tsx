b80e5e271505d0c92637489eec480459bbc1b190
machida4 <machida@kmc.gr.jp>
2023-06-12 18:17:43 +0900

    選んだ絵文字と一緒にpostできるように

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
-import EmojiPicker from "emoji-picker-react";
+import EmojiPicker, {
+  EmojiStyle,
+  EmojiClickData,
+  Emoji,
+} from "emoji-picker-react";
 import "./App.css";
 
-const App = () => {
-  const [posts, setPosts] = useState<string[]>([]);
+type Post = {
+  emoji: string;
+  content: string;
+};
 
-  const handleSubmit = (formText: string) => {
+const App = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const handleSubmit = (formText: Post) => {
     if (posts.length >= 10) {
       setPosts([...posts.slice(1), formText]);
     } else {
@@ -14,7 +23,7 @@ const App = () => {
   };
 
   const postBlocks = posts.map((post) => {
-    return <PostBlock content={post} />;
+    return <PostBlock post={post} />;
   });
 
   return (
@@ -25,36 +34,87 @@ const App = () => {
   );
 };
 
-const PostBlock: React.FC<{ content: string }> = (props: {
-  content: string;
-}) => {
-  return <div>{props.content}</div>;
+const PostBlock: React.FC<{ post: Post }> = (props: { post: Post }) => {
+  return (
+    <div>
+      <Emoji
+        unified={props.post.emoji}
+        emojiStyle={EmojiStyle.TWITTER}
+        size={22}
+      />
+      {props.post.content}
+    </div>
+  );
 };
 
-const ChatForm = (props: { onSubmit: (formText: string) => void }) => {
+const EmojiSelectBox = (props: { onSetSelectedEmoji: (emoji: string) => void}) => {
+  const [selectedEmoji, setSelectedEmoji] = useState<string>("");
+  const [shouldShowEmojiPicker, setShouldShowEmojiPicker] = useState<any>(true);
+
+  const onEmojiClick = (emojiClickData: EmojiClickData, event: MouseEvent) => {
+    setSelectedEmoji(emojiClickData.unified);
+    props.onSetSelectedEmoji(emojiClickData.unified);
+  };
+
+  const handleClick = () => {
+    setShouldShowEmojiPicker(!shouldShowEmojiPicker);
+  };
+
+  return (
+    <div>
+      {selectedEmoji ? (
+        <div>
+          <Emoji
+            unified={selectedEmoji}
+            emojiStyle={EmojiStyle.TWITTER}
+            size={22}
+          />
+        </div>
+      ) : null}
+      {shouldShowEmojiPicker ? (
+        <EmojiPicker
+          onEmojiClick={onEmojiClick}
+          emojiStyle={EmojiStyle.TWITTER}
+          skinTonesDisabled={true}
+        />
+      ) : null}
+    </div>
+  );
+};
+
+const ChatForm = (props: { onSubmit: (post: Post) => void }) => {
+  const [emoji, setEmoji] = useState("");
   const [formText, setFormText] = useState("");
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleEmojiChange = (emoji: string) => {
+    setEmoji(emoji);
+  };
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormText(event.target.value);
   };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    props.onSubmit(formText);
+    const post = { emoji: emoji, content: formText };
+    props.onSubmit(post);
     setFormText("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        <input
-          type="text"
-          value={formText}
-          onChange={(event) => handleChange(event)}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
+    <div>
+      <EmojiSelectBox onSetSelectedEmoji={handleEmojiChange}/>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            value={formText}
+            onChange={(event) => handleTextChange(event)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   );
 };
 
